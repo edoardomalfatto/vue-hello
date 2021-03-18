63752c62acfb51136487637d1de5ea2f87eca3c6
Edoardo <edoardo.malfatto@hotmail.it>
2021-03-18 14:33:17 +0100

    completato con bonus

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        message: 'Un messaggio',
+        image: "https://miro.medium.com/max/6416/1*7OCwu--TWqVluPMsZdzWKw.png",
+        alt_image: "VueJs Logo"
+    }
+});
